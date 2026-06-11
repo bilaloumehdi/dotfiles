fe6f374d766324a3f9a2f71fb47deaebc0cb34a3
Bilal Oumehdi <bilal.oumehdi@oracle.com>
2026-06-11 19:33:21 +0100

    feat: add neovim config, options,keymaps,plugins

---
+++ .config/nvim/lua/plugins/colortheme.lua
@@ -0,0 +1,6 @@
+vim.pack.add({
+	{ src = "https://github.com/catppuccin/nvim", name="catppuccin" },
+})
+
+--colorscheme catppuccin-nvim " catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
+vim.cmd.colorscheme 'catppuccin-mocha'
